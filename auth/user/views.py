1b4573eaedfa675c9390063ee1edf80f7798ac53
Manuel-bit <emmanuel_njuguna@outlook.com>
2020-03-28 11:28:24 +0300

    add user creation form

--- auth/user/views.py
+++ auth/user/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
 
 # Create your views here.
 def signUp(request):
+    if request.method == "post":
+        form = UserCreationForm(request.post)
+        if form.is_valid():
+            username=form.cleaned_data.get("username")
+            messages.success(request, f'Account created for {{username}}! ')
+            return redirect('#')
 
-    return render(request, "user/signup.html")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "user/signup.html", {'form':form})
